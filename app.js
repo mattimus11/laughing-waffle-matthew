426c2e75a6fc2155d6f2ade3f02aed92f6f6aece
-mattimus11 <matthewhowell64@gmail.com>
2024-08-29 13:43:42 -0500

    addes ejs

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
-
+app.set('view engine','ejs')
 app.use(express.static('./public/'))
 
 console.log("whats up")
 
 app.get('/', function (req, res) {
   res.sendFile('/index.html')
+})
+
+
+app.get('/ejs',(res,req)=>{
+
+  res.render('index', {
+    myServerVariable :  'my server variable'
+  });
+
 })
 
 app.listen(5500)
