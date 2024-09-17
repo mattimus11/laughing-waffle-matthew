0a78c3fe9fa79a58912f1acd5de9d4a551fdcc79
Howell <mhowell1@una.edu>
2024-09-17 13:24:57 -0500

    added the update functio0n as well as delete function

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://Matthew:${process.env.mongo_pwd}@cluster0.vza6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = process.env.mongo_uri;
-
-app.set('view engine', 'ejs');
-app.use(express.static('./public/'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
 
 console.log(uri);
 
@@ -19,18 +22,20 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function connectToMongoDB() {
+
+async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Successfully connected to MongoDB!");
-  } catch (err) {
-    console.error("Error connecting to MongoDB", err);
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
-
-
-connectToMongoDB().catch(console.dir);
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -39,30 +44,62 @@ app.get('/', (req, res) => {
 });
 
 app.get('/read', async (req, res) => {
-    await client.connect()
-    let result = await client.db("matts-db").collection("cool-collection")
-      .find({}).toArray();
+  await client.connect()
+  let result = await client.db("matts-db").collection("cool-collection")
+    .find({}).toArray();
 
-    console.log(result);
+  console.log(result);
 
-    res.render('mongo', {
-      mongoResult: result
-    });
+  res.render('mongo', {
+    mongoResult: result
+  });
 });
 
-app.get('/insert',async(req,res)=>{
+app.get('/insert', async (req,res)=> {
 
-  res.render('insert')
+  console.log('in /insert');
+  //connect to db,
+  await client.connect();
+  //point to the collection 
+  await client.db("matts-db").collection("cool-collection").insertOne({ post: '1'});
+  await client.db("matts-db").collection("cool-collection").insertOne({ post: '2'});  
+  //insert into it
+  res.render('insert');
 
-  await client.connect()
-    let result = await client.db("matts-db").collection("cool-collection")
-      .insertOne({post:'harcoded'});
-    let result1 = await client.db("matts-db").collection("cool-collection")
-      .insertOne({post:'harcoded too'});
+}); 
 
-    console.log(result);
+app.post('/update/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("matts-db").collection("cool-collection");
+  let result = await collection.findOneAndUpdate( 
+  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
+)
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+}); 
+
+app.post('/delete/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("matts-db").collection("cool-collection");
+  let result = await collection.findOneAndDelete( 
+  {"_id": new ObjectId(req.params.id)})
+
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
 })
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  //insert into it
+
 });
+
+
+app.listen(5500);
