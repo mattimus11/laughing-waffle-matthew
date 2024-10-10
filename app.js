2be9ad823863a8779b7f3b4a44f7bfcd97b3a59a
mattimus11 <matthewhowell64@gmail.com>
2024-10-10 01:56:18 -0500

    should be fixed

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const { ObjectId } = require('mongodb');
+const {ObjectId} = require('mongodb');
 const path = require('path');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {MongoClient,ServerApiVersion} = require('mongodb');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const uri = `mongodb+srv://Matthew:${process.env.mongo_pwd}@cluster0.vza6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-// Initialize MongoDB client and connect once
+// connect to mongo
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -20,30 +22,37 @@ const client = new MongoClient(uri, {
 
 client.connect().then(() => {
     console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
 });
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
 
-// Routes
+//route to index
 app.get('/', async (req, res) => {
     const collection = client.db('matts-db').collection('cool-collection');
-    const movies = await collection.find().sort({ rating: -1 }).limit(5).toArray();
-    res.render('index', { movies });
+    const movies = await collection.find()
+    .sort({
+        rating: -1
+    }).limit(5)
+    .toArray();
+    res.render('index', {
+        movies
+    });
 });
-
+//route to ratings page
 app.get('/ratings', async (req, res) => {
     const collection = client.db('matts-db').collection('cool-collection');
-    const result = await collection.find().toArray();
-    res.render('ratings', { mongoResult: result });
+    const result = await collection.find()
+    .toArray();
+    res.render('ratings', {
+        mongoResult: result
+    });
 });
-
+//route to post the rating and make sure its a number
 app.post('/add-rating', async (req, res) => {
-    const { movieName, rating } = req.body;
+    const {
+        movieName,
+        rating
+    } = req.body;
+    //number not string iss17
     const numericRating = parseFloat(rating);
 
     if (isNaN(numericRating) || numericRating < 1 || numericRating > 10) {
@@ -57,25 +66,29 @@ app.post('/add-rating', async (req, res) => {
     });
     res.redirect('/ratings');
 });
-
+//update the rating
 app.post('/update/:id', async (req, res) => {
     const movieId = req.params.id;
     const newRating = parseFloat(req.body.newRating);
     const collection = client.db('matts-db').collection('cool-collection');
-    await collection.updateOne(
-        { _id: new ObjectId(movieId) },
-        { $set: { rating: newRating } }
-    );
+    await collection.updateOne({
+        _id: new ObjectId(movieId)
+    }, {
+        $set: {
+            rating: newRating
+        }
+    });
     res.redirect('/ratings');
 });
-
+//remove the movie all
 app.post('/delete/:id', async (req, res) => {
     const collection = client.db('matts-db').collection('cool-collection');
-    await collection.findOneAndDelete({ _id: new ObjectId(req.params.id) });
+    await collection.findOneAndDelete({
+        _id: new ObjectId(req.params.id)
+    });
     res.redirect('/ratings');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
